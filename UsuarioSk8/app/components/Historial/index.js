82ff8e5f30fc7be34a725e0ff7d3fe6c3ce01826
Eduardo Llamas <edc_llamas@outlook.com>
2018-11-22 21:11:58 -0600

    Estilos de agenda, calendario, historial

--- UsuarioSk8/app/components/Historial/index.js
+++ UsuarioSk8/app/components/Historial/index.js
@@ -1,15 +1,46 @@
-import React, {Component} from 'react';
-import {View,Image,Text} from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body, Right } from 'native-base';
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import styles from './styles';
 
-class Historial extends Component{
-    render(){
-        return(
-            <View>
-                <Text>Hola desde Historial</Text>
-            </View>
-        )
-    }
+export default class Historial extends Component {
+  render() {
+      const img = require('../../assets/sk8_f9.jpg');
+      const img_icon = require('../../assets/sk8_icon2.png');
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card style={styles.tarjeta}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={img_icon} />
+                <Body>
+                  <Text>Venta</Text>
+                  <Text note>Fecha de la venta</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Image source={img} style={styles.bannerCat}/>
+                <Text>
+                  Descripción de la venta realizada
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent textStyle={styles.boton}>
+                  <Icon name="cart" />
+                  <Text>Costo</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
 }
 
-export default Historial

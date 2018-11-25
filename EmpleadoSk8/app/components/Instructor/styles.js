a4c1a3c08396d693ecfdc0e970b00aeb9e50c018
Johan Paredes <jooaanmix@gmail.com>
2018-11-24 19:30:07 -0600

    estilo de home,dashboard,usuario,paquete,tutor,clientes

---
+++ EmpleadoSk8/app/components/Instructor/styles.js
@@ -0,0 +1,63 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: null, height: null,
+    
+    
+  } ,
+  overlayContainer:{
+    flex :1,
+    backgroundColor:'rgba(0, 0, 0, 0.5)',
+    width : 410,
+  },
+  
+  lab:{
+    margin :5,
+    fontSize: 18,
+    color: '#FFFFFF',
+  },
+  texiput:{
+    width :350,
+    marginLeft:20,
+    marginRight:20,
+    backgroundColor:'rgba(229, 145, 11, 0.7)',
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    color : '#0D47A1',
+    fontSize:15,
+    alignSelf: 'center'
+  },
+  
+  parent:{
+    flex:1,
+    justifyContent:'center'
+  },
+  
+  titulo:{
+    color : '#FFFFFF',
+    textAlign: 'center',
+    marginVertical: 10,
+    fontSize: 25
+  },
+  emlogo:{
+    marginTop: 20,
+    marginRight:10,
+    width: 150, height: 60,
+   
+  },
+  
+  bulog:{
+    marginTop :25,
+    backgroundColor: '#003366',
+  },
+  resusu:{
+    fontSize :20,
+    color:'#FFFFFF'
+  }
+
+})
--- EmpleadoSk8/app/components/Usuario/index.js
+++ EmpleadoSk8/app/components/Usuario/index.js
@@ -1,34 +1,42 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native';
-import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
-
+import {View,Text,Image,ImageBackground} from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label,Button } from 'native-base';
+import styles from './styles'
 class Usuario extends Component{
   render(){
     return(
-      <Container>
-        <Header />
-        <Content>
+      < ImageBackground   style={styles.container}
+      source={require('../../image/Em.jpg')} >
+        <Content style={styles.overlayContainer}>
+        <Image
+        style={styles.emlogo}
+          source={require('../../image/logo1.png')}
+        />
+          <Text style={styles.titulo}>Registro de Usuario</Text>
           <Form>
-            <Item stackedLabel>
-              <Label>Nombre</Label>
-              <Input />
-            </Item>
+           
+              <Label style={styles.lab}>Nombre</Label>
+              <Input style={styles.texiput} secureTextEntry={true} placeholder="nombre" placeholderTextColor="#FFFFFF" onChangeText={text => this.setState({nombre:text})}/>
+           
 
-            <Item stackedLabel last>
-              <Label>Edad</Label>
-              <Input />
-            </Item>
-            <Item stackedLabel last>
-              <Label>Correo</Label>
-              <Input />
-            </Item>
-            <Item stackedLabel last>
-              <Label>Telefono</Label>
-              <Input />
-            </Item>
+             
+              <Label style={styles.lab} >Edad</Label>
+              <Input style={styles.texiput} secureTextEntry={true} placeholder="Edad" placeholderTextColor="#FFFFFF" onChangeText={text => this.setState({edad:text})} />
+           
+            
+              <Label style={styles.lab} >Correo</Label>
+              <Input style={styles.texiput} secureTextEntry={true} placeholder="correo" placeholderTextColor="#FFFFFF" onChangeText={text => this.setState({correo:text})} />
+           
+              <Label style={styles.lab}>Telefono</Label>
+              <Input style={styles.texiput} secureTextEntry={true} placeholder="Telefono" placeholderTextColor="#FFFFFF" onChangeText={text => this.setState({tel:text})}/>
+          
+
+           <Button style={styles.bulog} block onPress={_ => this.checkLogin()}>
+            <Text style={styles.resusu}> Registrar Usuario</Text>
+          </Button>
           </Form>
         </Content>
-      </Container>
+        </ ImageBackground>
     )
   }
 }
